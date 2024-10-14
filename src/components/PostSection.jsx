bc3709d9b40dda66459734aefdc3d6ad77e01b2c
emilianogastaldo <emiliano0303@gmail.com>
2024-10-14 16:13:22 +0200

    aggiunta la possiblità di cancellazione

--- src/components/PostSection.jsx
+++ src/components/PostSection.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const PostSection = () => {
     const [posts, setPosts] = useState([])
@@ -7,20 +7,32 @@ const PostSection = () => {
         try{
             const res = await axios.get('http://localhost:8000/posts');
             const fetchedPosts = res.data.posts;
-            console.log(fetchedPosts);
             setPosts(fetchedPosts);
         }catch(err){
             console.error(err);
         }
     }
+    const deletePost = async (id) => {
+
+        try{
+            const res = await axios.delete(`http://localhost:8000/posts/${id}`);
+        }catch(err){
+            console.error(err);
+        }finally{
+            fetchPosts();
+        }
+    }
+    useEffect(()=>{
+        fetchPosts()
+    },[])
      return (
      <>
        <section>
        <button onClick={fetchPosts} >Raccogli post</button>
-       <button onClick={()=> {setPosts([])}} >Elimina tutto</button>
+       <button onClick={()=> {setPosts([])}} >Nascondi lista</button>
        <ul>
             {posts.map((post)=>(
-                <li key={`${post.id}`}>{post.title}</li>
+                <li key={`${post.id}`}>{post.title} <button onClick={()=>(deletePost(post.id))}>Elimina post</button></li>
             ))}
         </ul>
 
