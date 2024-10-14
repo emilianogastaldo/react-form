11251907225de0ac3896a89fdcf8839b05443686
emilianogastaldo <emiliano0303@gmail.com>
2024-10-14 15:50:18 +0200

    tolgo l'esempio dei todo e metto i post dalle mie api

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,11 @@ import { createRoot } from 'react-dom/client'
 import Form from './components/Form'
 import './index.css'
 import ButtonToDo from './components/ButtonToDo'
+import PostSection from './components/PostSection'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Form/>
-    <ButtonToDo/>
+    <PostSection/>
   </StrictMode>,
 )
